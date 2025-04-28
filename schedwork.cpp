d2aa93c4e7a232adb1a5ea457c98b2677ff292d1
Kyle Lotke <lotke@usc.edu>
2025-04-28 00:17:03 +0000

    final push

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,9 +21,65 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool set_shifts(const AvailabilityMatrix& AV_matrix,
+    size_t daily_shifts,
+    size_t max_shifts,
+    size_t curr_day,
+    size_t shift,
+    DailySchedule& schedule,
+    map<Worker_T, int>& shifts_left);
 
 // Add your implementation of schedule() and other helper functions here
+
+bool set_shifts(const AvailabilityMatrix& AV_matrix,
+    size_t daily_shifts,
+    size_t max_shifts,
+    size_t curr_day,
+    size_t shift,
+    DailySchedule& schedule,
+    map<Worker_T, int>& shifts_left)
+{
+    
+    if (curr_day == AV_matrix.size()) { // base case: all days are scheduled
+        return true;
+    }
+
+    //  assing each worker to the current shift
+    for (Worker_T worker_id = 0; worker_id < (Worker_T)AV_matrix[curr_day].size(); ++worker_id) {
+        if (AV_matrix[curr_day][worker_id] && shifts_left[worker_id] > 0) {
+
+            schedule[curr_day][shift] = worker_id;
+            shifts_left[worker_id]--;
+
+            size_t next_day;
+            if (shift + 1 == daily_shifts) {
+                next_day = curr_day + 1;
+            }
+            else {
+                next_day = curr_day;
+            }
+
+            size_t next_shift;
+            if (shift + 1 == daily_shifts) {
+                next_shift = 0;
+            }
+            else {
+                next_shift = shift + 1;
+            }
+
+            if (set_shifts(AV_matrix, daily_shifts, max_shifts, next_day, next_shift, schedule, shifts_left)) {
+                return true;
+            }
+
+            schedule[curr_day][shift] = INVALID_ID;
+            shifts_left[worker_id]++;
+        }
+    }
+
+    // can't find valid assignment
+    return false;
+}
+
 
 bool schedule(
     const AvailabilityMatrix& avail,
@@ -37,9 +93,17 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    const size_t days = avail.size();
+    const size_t workers = avail[0].size();
+    sched.resize(days);                                
+    for (size_t day = 0; day < days; day++) {
+      sched[day].resize(dailyNeed, INVALID_ID);         
+    }
+    map<Worker_T, int> shifts_left;
+    for (Worker_T worker = 0; worker < workers; worker++){
+      shifts_left[worker] = maxShifts;
+    }
 
-
-
-
+    return set_shifts(avail,dailyNeed,maxShifts,0,0,sched,shifts_left);
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,9 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void gen_possibilties(string& partial_word, int indx, 
+int missing_letters, string included_letters, 
+const set<string>& dict, set<string>& possibilities);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +24,52 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    set<string> possible_words;
+    int word_len = in.size();
+    int num_dashes = 0;
+    for(int i=0; i < word_len; i++) {
+        if(in[i] == '-') num_dashes++;
+    }
 
+    string curr = in;
+    gen_possibilties(curr, 0, num_dashes, floating, dict, possible_words);
+
+    return possible_words;
 }
 
 // Define any helper functions here
+
+void gen_possibilties(string& partial_word, int indx, 
+int missing_letters, string included_letters, 
+const set<string>& dict, set<string>& possibilities) {
+    if(indx == partial_word.size()) {
+        if(dict.count(partial_word) && included_letters.empty()) {
+            possibilities.insert(partial_word);
+            return;
+        }
+        return;
+    }
+
+    if(partial_word[indx] != '-') { // if letter is known go to next and recurse
+        gen_possibilties(partial_word, indx + 1, missing_letters, included_letters, dict, possibilities);
+        return;
+    }
+
+    for(char letter = 'a'; letter <= 'z'; letter++) {
+        string included_updated = included_letters;
+        int letter_indx = included_updated.find(letter);
+        if(letter_indx != string::npos) {
+            included_updated.erase(letter_indx, 1);
+        }
+        else if(missing_letters <= (int)included_letters.size()) {
+            continue;
+        }
+
+        partial_word[indx] = letter;
+        gen_possibilties(partial_word, indx + 1, missing_letters - 1, included_updated, dict, possibilities);
+
+    }
+
+    partial_word[indx] = '-';
+
+}
